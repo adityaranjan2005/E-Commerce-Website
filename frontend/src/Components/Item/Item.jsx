257b70596caf24dfebf2b5738c4ee54621da7233
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-02-25 00:37:13 +0530

    Update Item.jsx

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,34 +1,22 @@
-import React, { useContext } from 'react'
-import './CSS/ShopCategory.css'
-import { ShopContext } from '../Context/ShopContext'
-import dropdown_icon from '../Components/Assets/dropdown_icon.png'
-import Item from '../Components/Item/Item'
+import React from 'react'
+import './Item.css'
 
-const ShopCategory = (props) => {
-  const {all_product} = useContext(ShopContext)
+
+const Item = (props) => {
   return (
-    <div className='shop-category'>
-      <img src={props.banner} alt=''/>
-      <div className='shopcategory-indexSort'>
-        <p>
-          <span>Showing 1-12</span> out of 36 products
-        </p>
-        <div className='shopcategory-sort'>
-          Sort by <img src={dropdown_icon} alt='' />
+    <div className='item'>
+      <img src={props.image} alt='' />
+      <p>{props.name}</p>
+      <div className='item-prices'>
+        <div className='item-price-new'>
+          ${props.new_price}
         </div>
-      </div>
-      <div className='shopcategory-products'>
-        {all_product.map((item,i)=>{
-          if (props.category===item.category) {
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-          }
-          else{
-            return null;
-          }
-        })}
+        <div className='item-price-old'>
+          ${props.old_price}
+        </div>
       </div>
     </div>
   )
 }
 
-export default ShopCategory
+export default Item
