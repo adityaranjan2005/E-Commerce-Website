46daf8bf5a5c58dbc7d20090a2435c9f63daf36a
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-02-25 00:44:56 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import Shop from './Pages/Shop';
 import Product from './Pages/Product.jsx';
 import Cart from './Pages/Cart.jsx';
 import LoginSignup from './Pages/LoginSignup.jsx';
+import Footer from './Components/Footer/Footer.jsx';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
 
 
@@ -16,15 +20,16 @@ function App() {
       <Navbar/>  
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
         <Route path="/product" element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>          
         </Route>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
       </Routes> 
+      <Footer/>
       </BrowserRouter>
     </div>
   );
