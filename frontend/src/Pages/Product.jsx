0fe40ac22b8b9fa786eeaddea3a7eadf43edea2f
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-03-02 15:50:33 +0530

    Linked Breadcrum and ProductDisplay to product page

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
@@ -8,7 +10,8 @@ const Product = () => {
   const product = all_product.find((e)=> e.id === Number(productId))
   return (
     <div>
-     
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product} />
     </div>
   )
 }
