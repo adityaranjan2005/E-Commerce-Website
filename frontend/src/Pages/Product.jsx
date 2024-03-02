8b38d363b32660ab6edd59b12d20b66b5ace23e4
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-03-02 18:51:13 +0530

    Added Description Box properties to product page

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -3,6 +3,8 @@ import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
@@ -10,8 +12,9 @@ const Product = () => {
   const product = all_product.find((e)=> e.id === Number(productId))
   return (
     <div>
-      <Breadcrum product={product}/>
+      <Breadcrum product={product} />
       <ProductDisplay product={product} />
+      <DescriptionBox />
     </div>
   )
 }
