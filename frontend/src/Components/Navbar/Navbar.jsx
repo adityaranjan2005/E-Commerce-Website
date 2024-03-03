adc5abaa363717d7dd8d30e9e215366d4f0d48e9
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-03-03 21:03:01 +0530

    called Items in cart feature

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-
-
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { ShopContext } from '../../Context/ShopContext';
+
+
 export const Navbar = () => {
 
   const [menu,setMenu] = useState("shop");
+  const {getTotalCartItems} = useContext(ShopContext)
 
   return (
     <div className="navbar">
@@ -24,7 +26,7 @@ export const Navbar = () => {
       <div className="nav-login-cart">
         <Link to='/login'><button>Login</button></Link>
         <Link to='/cart'><img src={cart_icon} alt=""/></Link>
-        <div className="nav-cart-count">0</div>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
   )
