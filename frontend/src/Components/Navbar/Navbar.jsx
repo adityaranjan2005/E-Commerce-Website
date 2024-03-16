26d3c60051084e3491cc335586592f0accc06a08
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-03-16 14:33:38 +0530

    Changed Name

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>SHOPPER</p>
+        <p>Clothify</p>
       </div>
       <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt=''/>
       <ul ref={menuRef} className="nav-menu">
