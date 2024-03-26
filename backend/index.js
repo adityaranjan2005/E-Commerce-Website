38b85e0804a5e78aa13a34d4c7681ec4b672783e
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-03-27 01:24:45 +0530

    Created different API for removing products, adding products, etc

--- backend/index.js
+++ backend/index.js
@@ -212,7 +212,60 @@ app.get('/newcollections',async (req,res) =>{
   res.send(newcollection);
 })
 
+//creating endpoint for popular in women section
+app.get('/popularinwomen', async(req,res)=>{
+  let products = await Product.find({category:"women"});
+  let popular_in_women = products.slice(0,4);
+  console.log("Popular in Women Fetched")
+  res.send(popular_in_women);
+})
 
+// creating middleware to fetch users
+
+  const fetchUser = async (req,res,next) => {
+    const token = req.header('auth-token');
+    if (!token) {
+      res.status(401).send({errors:"Please authenticate using valid token."})
+    }
+    else{
+      try {
+        const data = jwt.verify(token,'secret_ecom');
+        req.user = data.user;
+        next();
+      } catch (error) {
+        res.status(401).send({errors: "please authenticate using a valid token"})
+      }
+    }
+  }
+
+
+//creating endpoint for adding products in cartdata
+app.post('/addtocart', async ()=>{
+  console.log('added',req.body.itemId);
+  let userData = await Users.findOne({_id: req.user.id});
+  userData.cartData[req.body.itemId] += 1;
+  await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+  res.send("Added")
+})
+
+// craeting endpoint to remove the product from cart data
+app.post('/removefromcart',fetchUser,async (req,res) => {
+  console.log('removed',req.body.itemId);
+  let userData = await Users.findOne({_id: req.user.id});
+  if(userData.cartData[req.body.itemId]>0)
+  userData.cartData[req.body.itemId] -= 1;
+  await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+  res.send("Removed")
+})
+
+//creating endpoint to nget cart data
+
+app.post('/getcart',fetchUser,async (req,res) =>{
+  console.log("GetCart");
+  let userData = await Users.findOne({_id:req.user.id})
+  res.json(userData.cartData);
+})
+ 
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server running on port " + port);
